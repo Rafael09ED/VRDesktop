c093c77594f4d853c7ec7546e3cb69effb72e80a
Rafael09ED <starmarstrek@gmail.com>
2014-10-21 18:58:41 -0400

    Timetracker added & RM
    added timetracker

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+Virtual (Reality) Desktop: VRDesktop : VRD :VD
+
+By: Rafael09ED, AddTheLetters, and BlueDevil
+
+The goal of this program is to create a virtual desktop to allow VR devices to interact with you desktop via a virtual desktop.
+
+
+To use the source, you need JNA, LWJGL, commonsLang3, and Slick?
---
+++ src/tools/timeTracker.java
@@ -0,0 +1,27 @@
+
+public class timeTracker {
+	long timeCreated;
+	double[] timeSteps;
+	int stepPos;
+	public timeTracker(int maxArraySizeIn) {
+		timeSteps = new double[maxArraySizeIn];
+		timeCreated = System.nanoTime();
+		stepPos = 0;
+	}
+	public void nextStep(){
+		timeSteps[stepPos] = System.nanoTime();
+		stepPos++;
+	}
+	public double[] timePerStep(){
+		double[] timePerStep = new double[timeSteps.length];
+		
+		timePerStep[0] = (timeSteps[0]-timeCreated)/1000000000.0;
+		
+		for (int i = 1; i < stepPos; i++) {
+			
+			timePerStep[i] = (timeSteps[i]-timeSteps[i-1])/1000000000.0;
+		}
+		
+		return timePerStep;
+	}
+}
